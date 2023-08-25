898a888bff9cbc9f8aacf5140d80b8d8255b4179
TanweerAhsan11 <hasantanveer624@gmail.com>
2023-08-25 20:27:49 +0530

    Binary Search in recursive.

---
+++ binarySearchRecursive.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class binarySearchRecursive {
+    public static int binarySearch(int arr[], int key, int low,int high)
+    {
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]==key)
+            {
+                return mid;
+            }
+            else if(arr[mid]>key)
+            {
+               return binarySearch(arr, key, low,mid-1);
+            }
+            else{
+                return binarySearch(arr, key, mid+1, high);
+            }
+        }
+        return -1;
+    }
+     public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        int low=0;
+        int high=n-1;
+        int key=s.nextInt();
+        binarySearch(arr,key,low,high);
+        System.out.println(binarySearch(arr,key,low,high));
+    }
+}
