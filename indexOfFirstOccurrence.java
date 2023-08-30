cac1aaa7cedc7196f964e80cd9c75148c4e6894e
TanweerAhsan11 <hasantanveer624@gmail.com>
2023-08-30 18:49:04 +0530

    Index of 1st element occurrence.

---
+++ indexOfFirstOccurrence.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class indexOfFirstOccurrence {
+    public static int firstOccurrence(int arr[],int key)
+    {
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==key)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        int key=s.nextInt();
+        int c=firstOccurrence(arr,key);
+        System.out.println(c);
+    }
+}
