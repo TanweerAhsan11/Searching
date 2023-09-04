a9a94cf1e48d5cc2599855bf74451e5e5742baf5
TanweerAhsan11 <hasantanveer624@gmail.com>
2023-09-04 18:56:15 +0530

    Two pointer approach with naive solution.

---
+++ twoPointerApproach.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class twoPointerApproach {
+    public static boolean twoPair(int arr[],int key)
+    {
+        int n=arr.length;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i] + arr[j]==key)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        int key=s.nextInt();
+        boolean c=twoPair(arr,key);
+        System.out.println(c);
+    }
+}
